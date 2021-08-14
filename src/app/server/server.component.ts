8df5dba697320a59c90af66bfd047970cf7fad96
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-14 23:47:21 +0530

    add: added function for getting server status and changed the visibility

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
   serverId: number = Math.round(Math.random() * 10e6);
-  serverStatus: string = ['online', 'offline'][Math.round(Math.random() * 1)];
+  private serverStatus: string = ['online', 'offline'][
+    Math.round(Math.random() * 1)
+  ];
+
+  getServerStatus = () => {
+    return this.serverStatus;
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
