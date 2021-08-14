7016ff822dd154147ca76ae456d4a9194c49e7f4
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-14 23:38:44 +0530

    add: added dynamic data binding to server

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.scss']
+  styleUrls: ['./server.component.scss'],
 })
 export class ServerComponent implements OnInit {
+  serverId: number = Math.round(Math.random() * 10e6);
+  serverStatus: string = ['online', 'offline'][Math.round(Math.random() * 1)];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
