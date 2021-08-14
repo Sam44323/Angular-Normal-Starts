c2c002fa19b54a78262583e2bede566d87dc9faa
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-14 21:38:48 +0530

    update: leaned the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  name = 'Sam Henrick';
-}
+export class AppComponent {}
