2a409c8009fbd6a6001be9e86941df0ab2e2ceea
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-14 22:52:34 +0530

    add: added a nested server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [AppComponent, ServersComponent],
+  declarations: [AppComponent, ServersComponent, ServerComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
