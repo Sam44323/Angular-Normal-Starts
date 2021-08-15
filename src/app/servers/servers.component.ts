d8b2fe455a2dff423c3eb8c88cdf5367ff8be2f4
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-15 14:26:59 +0530

    add: added input changer event handler for server name

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   addNewServer: boolean = false;
   serverCreationStatus = 'No server was created!';
+  serverInputName = '';
 
   constructor() {
     setTimeout(() => {
@@ -19,5 +20,9 @@ export class ServersComponent implements OnInit {
 
   onServerCreation = () => {
     this.serverCreationStatus = 'Server was created!';
+  };
+
+  onServerInputChange = (e: any) => {
+    this.serverInputName = e.target.value;
   };
 }
